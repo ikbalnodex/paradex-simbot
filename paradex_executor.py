917da688caee77fc24d3b88e4e2012a01b56f7de
Ikbal Nodex <119092888+ikbalnodex@users.noreply.github.com>
2026-03-14 06:51:02 +0700

    Update paradex_executor.py

--- paradex_executor.py
+++ paradex_executor.py
@@ -237,6 +237,8 @@ class ParadexExecutor:
         try:
             logger.info(f"Placing order: {side} {size} {market} @ {order_type}")
             from paradex_py.common.order import Order, OrderSide, OrderType
+            logger.info(f'OrderSide values: {list(OrderSide)}')
+            logger.info(f'OrderType values: {list(OrderType)}')
             order_obj = Order(
                 market=market,
                 side=OrderSide[side.upper()],
